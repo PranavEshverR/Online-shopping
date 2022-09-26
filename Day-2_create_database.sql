a40c4bb1ca60733162b8ebfcc652cdac5e9104bb
PranavEshverR <pranav.eshver@indiumsoft.com>
2022-09-26 12:08:45 +0530

    Create Day-2_create_database.sql

---
+++ Day-2_create_database.sql
@@ -0,0 +1,12 @@
+create database sql_training;
+show databases;
+use sql_training;
+create table sql_training_candidates_details (
+emp_id int not null auto_increment,
+name varchar(45) not null,
+designation varchar(35) not null,
+age int not null,
+primary key (emp_id)
+);
+show tables;
+ 
